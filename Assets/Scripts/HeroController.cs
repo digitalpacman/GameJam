6d77b0108cdf6230f8f4a7030313fba6209ac8cd
Josh McNary <josh.mcnary@gmail.com>
2018-11-20 13:43:16 -0800

    pathfinding poc

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HeroController : MonoBehaviour {
+
+    public NavMeshAgent Agent;
+
+	// Use this for initialization
+	void Start () {
+        Agent = GetComponent<NavMeshAgent>();
+        Agent.enabled = false;
+        Agent.enabled = true;
+        //Agent.SetDestination(new Vector3(5, 5, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        
+
+	}
+}
---
+++ Assets/Scripts/NavPlane.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NavPlane : MonoBehaviour, IPointerClickHandler {
+
+    public HeroController Hero;
+
+    public void OnPointerClick(PointerEventData pointerData) {
+        Vector3 worldPosition = pointerData.pointerCurrentRaycast.worldPosition;
+        Debug.Log(0);
+        if (Input.GetMouseButtonUp(0)) {
+            //
+        }
+        else if (Input.GetMouseButtonUp(1)) {
+            Hero.Agent.SetDestination(worldPosition);
+        }
+    }
+
+}
